bb134076997c3591295668a9610598b29b1eb51f
retaor <retaor@aliyun.com>
2022-09-15 09:46:32 +0800

    first commit

---
+++ src/main/java/com/retros/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.retros.git;
+
+/**
+ * @author retros
+ * @version 1.1
+ * 我的编程自学课，Java
+ * Hello world!
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
